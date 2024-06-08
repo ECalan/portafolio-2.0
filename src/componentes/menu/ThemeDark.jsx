413cd7702b082c9d57874a9b1f68d77278973e6b
ECalan <tecnoelectronictrance@gmail.com>
2024-06-08 16:35:39 -0600

    se agregaron estilos

--- src/componentes/menu/ThemeDark.jsx
+++ src/componentes/menu/ThemeDark.jsx
@@ -1,8 +1,10 @@
 const ThemeDark =()=>{
-    document.querySelector("#themes").innerHTML = "Theme Dark";
-    
+    document.querySelector("#themes").innerHTML = "Theme Light";
+    document.querySelector(".menu").style= "background-color: #212121;"
+    document.querySelector(".title__bio").style = "background-color: #212121;"
     document.querySelector("body").style =
-"background: 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom left, #adbadcff 0% 10%, #c3d2f8ff 10% 20%, #dbe1f0ff 20% 30%, #f1f3f8ff 30% 40%, #ffffffff 40% 50%), 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom right, #adbadcff 0% 10%, #c3d2f8ff 10% 20%, #dbe1f0ff 20% 30%, #f1f3f8ff 30% 40%, #ffffffff 40% 50%), 50% 50% / 100% 100% repeat radial-gradient(circle farthest-corner at 50% 50%, #ffffffff 100%, #000000ff 100%);";
+    "background: 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom left, #4e6ab1ff 0% 10%, #91a3cfff 10% 20%, #adbadbff 20% 30%, #d8deeeff 30% 40%, #f4f6faff 40% 50%), 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom right, #4e6ab1ff 0% 10%, #91a3cfff 10% 20%, #adbadbff 20% 30%, #d8deeeff 30% 40%, #f4f6faff 40% 50%), 50% 50% / 100% 100% repeat radial-gradient(farthest-corner at 50% 50%, #4dcd23ff 0%, #000000ff 100%);";
+
 };
 
 export default ThemeDark;
