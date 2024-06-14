e7c653fac3d27c4ef077166e7ff9a4c9ca503d4e
ECalan <tecnoelectronictrance@gmail.com>
2024-06-13 21:30:25 -0600

    le elimino código innecesario y se agrego un li al boton themes

--- src/componentes/menu/Menu.jsx
+++ src/componentes/menu/Menu.jsx
@@ -22,23 +22,15 @@ const Menu = () => {
             </div>
             <nav className="menu_content">
             <ul className="menu__list">
-                {/*<li className="menu__list__item" id="menu__list__item__theme">
-                    <a className="menu__list__item__icon" href="#Change__theme">
-                        <img className="menu__list_img" src="/img/icon_eddy.svg" alt="icono_theme"/>
-                        <select className="menu__list__themes" name="Themes" id="themes">
-                            <option value="Dark">Theme Dark</option>
-                            <option value="White">Theme White</option>
-    </select></a></li>*/}
-            <button id="themes"  className="menu__button__themes" onClick={()=> setValue(value === false)}>
+            <li><button id="themes"  className="menu__button__themes" onClick={()=> setValue(value === false)}>
                 Theme Light
-            </button> {value}
+            </button> {value}</li>
                 <li className="menu__list__item"><a href="#sobre-mi">Sobre mi</a></li>
                 <li className="menu__list__item"><a href="#skills">Skills</a></li>
                 <li className="menu__list__item"><a href="#hobbies">Hobbies</a></li>
                 <li className="menu__list__item"><a href="#formacion">Formación</a></li>
                 <li className="menu__list__item"><a href="#xp">Proyectos</a></li>
                 <li className="menu__list__item contacto"><a href="#contacto">@contacto</a></li>
-                <li className="menu__list__item__contacto"><a href="#contacto">@contacto</a></li>
             </ul>
         </nav>
         </header>
