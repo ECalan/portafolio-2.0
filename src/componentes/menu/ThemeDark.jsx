89bd6845c7533da6dfcc2e5978e22bb3e9bf8e09
ECalan <tecnoelectronictrance@gmail.com>
2024-06-21 18:05:34 -0600

    se agregaron estilos al los "a" y al div de biography

--- src/componentes/menu/ThemeDark.jsx
+++ src/componentes/menu/ThemeDark.jsx
@@ -1,12 +1,15 @@
 const ThemeDark =()=>{
     document.querySelector("#themes").innerHTML = "Theme Light";
     document.querySelector(".menu__header").style = "color: grey;";
-    document.querySelector("a").style = "color: green;";
+    document.querySelectorAll("a").forEach(function(e){
+        e.classList.remove("black", "green-hover");
+        e.classList.add("green");
+    });
     document.querySelector(".menu").style= "background-color: #212121;"
     document.querySelector(".title__bio").style = "background-color: #212121;"
     document.querySelector("body").style =
     "background: 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom left, #4e6ab1ff 0% 10%, #91a3cfff 10% 20%, #adbadbff 20% 30%, #d8deeeff 30% 40%, #f4f6faff 40% 50%), 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom right, #4e6ab1ff 0% 10%, #91a3cfff 10% 20%, #adbadbff 20% 30%, #d8deeeff 30% 40%, #f4f6faff 40% 50%), 50% 50% / 100% 100% repeat radial-gradient(farthest-corner at 50% 50%, #4dcd23ff 0%, #000000ff 100%);";
-
+    document.querySelector(".title__div").style ="background-color: rgba(255, 255, 255, 0.1);";
 };
 
 export default ThemeDark;
--- src/componentes/menu/ThemeLight.jsx
+++ src/componentes/menu/ThemeLight.jsx
@@ -1,11 +1,15 @@
 const ThemeLight =()=>{
     document.querySelector("#themes").innerHTML = "Theme Dark";
     document.querySelector(".menu__header").style = "color: black;";
-    document.querySelector("a").style = "color: black;";
+    document.querySelectorAll("a").forEach(function(e){
+        e.classList.remove("green");
+        e.classList.add("black", "green-hover");
+    });
     document.querySelector(".menu").style = "background-color: #e5e5f7; background-image:  linear-gradient(30deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(150deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(30deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(150deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(60deg, #d2d4fc77 25%, transparent 25.5%, transparent 75%, #d2d4fc77 75%, #d2d4fc77), linear-gradient(60deg, #d2d4fc77 25%, transparent 25.5%, transparent 75%, #d2d4fc77 75%, #d2d4fc77);"
     document.querySelector(".title__bio").style = "background-color: #e5e5f7; background-image:  linear-gradient(30deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(150deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(30deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(150deg, #d2d4fc 12%, transparent 12.5%, transparent 87%, #d2d4fc 87.5%, #d2d4fc), linear-gradient(60deg, #d2d4fc77 25%, transparent 25.5%, transparent 75%, #d2d4fc77 75%, #d2d4fc77), linear-gradient(60deg, #d2d4fc77 25%, transparent 25.5%, transparent 75%, #d2d4fc77 75%, #d2d4fc77);"
     document.querySelector("body").style =
-"background: 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom left, #adbadcff 0% 10%, #c3d2f8ff 10% 20%, #dbe1f0ff 20% 30%, #f1f3f8ff 30% 40%, #ffffffff 40% 50%), 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom right, #adbadcff 0% 10%, #c3d2f8ff 10% 20%, #dbe1f0ff 20% 30%, #f1f3f8ff 30% 40%, #ffffffff 40% 50%), 50% 50% / 100% 100% repeat radial-gradient(circle farthest-corner at 50% 50%, #ffffffff 100%, #000000ff 100%);";
+    "background: 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom left, #adbadcff 0% 10%, #c3d2f8ff 10% 20%, #dbe1f0ff 20% 30%, #f1f3f8ff 30% 40%, #ffffffff 40% 50%), 0% 0% / 100px 100px repeat repeating-linear-gradient(to bottom right, #adbadcff 0% 10%, #c3d2f8ff 10% 20%, #dbe1f0ff 20% 30%, #f1f3f8ff 30% 40%, #ffffffff 40% 50%), 50% 50% / 100% 100% repeat radial-gradient(circle farthest-corner at 50% 50%, #ffffffff 100%, #000000ff 100%);";
+    document.querySelector(".title__div").style ="background-color: rgba(0, 0, 0, 0.1);";
 };
 
 export default ThemeLight;
