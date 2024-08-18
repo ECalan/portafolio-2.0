c40cfffaad86842230b4eeee99af665258e88f29
ECalan <tecnoelectronictrance@gmail.com>
2024-08-17 18:22:34 -0600

    se renombraron las clases que contenian academic a academy

--- src/componentes/academy/Academy.jsx
+++ src/componentes/academy/Academy.jsx
@@ -1,57 +1,57 @@
 import "../academy/Academy.css";
 const Academy = () => {
   return (
-    <section className="academic" id="formacion">
-      <div className="academic__container">
-        <h2 className="academic__title">Formación académica</h2>
-        <div className="academic__courses" id="academic__all">
-          <div className="academic__courses__box">
-            <ul className="academic__courses__list">
-              <li className="academic__courses__item__img">
+    <section className="academy" id="formacion">
+      <div className="academy__container">
+        <h2 className="academy__title">Formación académica</h2>
+        <div className="academy__courses" id="academy__all">
+          <div className="academy__courses__box">
+            <ul className="academy__courses__list">
+              <li className="academy__courses__item__img">
                 <img
-                  className="academic__courses__item__img__logo"
+                  className="academy__courses__item__img__logo"
                   src="./img/Logo_Alura_Latam.png"
                   alt="Alura Latam"
                 />
               </li>
-              <li className="academic__courses__item__title">
+              <li className="academy__courses__item__title">
                 <h4>Formación Principiante en Programación</h4>
               </li>
-              <li className="academic__courses__item__subtitle">
+              <li className="academy__courses__item__subtitle">
                 <p>2023 - Alura Latam</p>
               </li>
             </ul>
           </div>
-          <div className="academic__courses__box">
-            <ul className="academic__courses__list">
-              <li className="academic__courses__item__img">
+          <div className="academy__courses__box">
+            <ul className="academy__courses__list">
+              <li className="academy__courses__item__img">
                 <img
-                  className="academic__courses__item__img__logo"
+                  className="academy__courses__item__img__logo"
                   src="./img/Logo_Alura_Latam.png"
                   alt="Alura Latam"
                 />
               </li>
-              <li className="academic__courses__item__title">
+              <li className="academy__courses__item__title">
                 <h4>Formación Front End G4 - ONE</h4>
               </li>
-              <li className="academic__courses__item__subtitle">
+              <li className="academy__courses__item__subtitle">
                 <p>2023 - Alura Latam</p>
               </li>
             </ul>
           </div>
-          <div className="academic__courses__box">
-            <ul className="academic__courses__list">
-              <li className="academic__courses__item__img">
+          <div className="academy__courses__box">
+            <ul className="academy__courses__list">
+              <li className="academy__courses__item__img">
                 <img
-                  className="academic__courses__item__img__logo academic__university"
+                  className="academy__courses__item__img__logo academy__university"
                   src="./img/Logo_Universidadrural-Guatemala.png"
                   alt="Universidad Rural de Guatemala"
                 />
               </li>
-              <li className="academic__courses__item__title">
+              <li className="academy__courses__item__title">
                 <h4>1 curso - Ing. Industrial</h4>
               </li>
-              <li className="academic__courses__item__subtitle">
+              <li className="academy__courses__item__subtitle">
                 <p>2023 - Universidad Rural de Guatemala</p>
               </li>
             </ul>
